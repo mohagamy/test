12d5fb20a3bd5ba9782955f8f890b98b5290cad2
Mohammed <mabdella@ualberta.ca>
2020-11-04 15:22:26 -0500

    Update Functional Programming in Python.py

--- Functional Programming in Python.py
+++ Functional Programming in Python.py
@@ -1,3 +1,7 @@
+# functional programming style avoids side-effects and changing state in your programs by focusing on the application of mathematical functions as its core primitive. 
+# we represent data inside our example program using immutable data structures (tuples and namedtuples from the "collections" module built into Python). 
+# Immutable data structures cannot be modified in-place and this can help reduce bugs.
+
 # Immutable Data Structures
 
 
